1a3d8a6b6397834ff06381b31d2035d721674629
sir-hector <krauskarol7@gmail.com>
2022-12-14 12:37:02 +0100

    add second part of 4

--- 4/index.js
+++ 4/index.js
@@ -4,22 +4,47 @@ var fs_1 = require("fs");
 var file = (0, fs_1.readFileSync)("./data.txt", "utf-8");
 var splitted = file.split("\n");
 var result = 0;
+var result2 = 0;
 splitted.forEach(function (element) {
     if (checkPairs(element)) {
         result++;
     }
+    if (checkIfAnyDuplicate(element)) {
+        result2++;
+    }
 });
 console.log(result);
+console.log(result2);
 function checkPairs(element) {
     var parts = element.split(',');
     var splittedFirst = parts[0].split('-');
     var splittedSecond = parts[1].split('-');
     if ((Number(splittedFirst[0]) >= Number(splittedSecond[0])) && Number((splittedFirst[1]) <= Number(splittedSecond[1]))) {
-        console.log("A ".concat(splittedFirst[0], " - ").concat(splittedFirst[1], " w ").concat(splittedSecond[0], " ").concat(splittedSecond[1]));
         return true;
     }
     if ((Number(splittedSecond[0]) >= Number(splittedFirst[0])) && Number((splittedSecond[1]) <= Number(splittedFirst[1]))) {
-        console.log("B ".concat(splittedFirst[0], " - ").concat(splittedFirst[1], " w ").concat(splittedSecond[0], " ").concat(splittedSecond[1]));
+        return true;
+    }
+    return false;
+}
+function checkIfAnyDuplicate(element) {
+    var parts = element.split(',');
+    var splittedFirst = parts[0].split('-');
+    var splittedSecond = parts[1].split('-');
+    if ((Number(splittedFirst[0]) >= Number(splittedSecond[0])) && Number((splittedFirst[0]) <= Number(splittedSecond[1]))) {
+        console.log("A ".concat(splittedFirst[0], " - ").concat(splittedFirst[1], " w ").concat(splittedSecond[0], " ").concat(splittedSecond[1]));
+        return true;
+    }
+    if ((Number(splittedFirst[1]) >= Number(splittedSecond[0])) && Number((splittedFirst[1]) <= Number(splittedSecond[1]))) {
+        console.log("A ".concat(splittedFirst[0], " - ").concat(splittedFirst[1], " w ").concat(splittedSecond[0], " ").concat(splittedSecond[1]));
+        return true;
+    }
+    if ((Number(splittedSecond[0]) >= Number(splittedFirst[0])) && Number((splittedSecond[0]) <= Number(splittedFirst[1]))) {
+        console.log("A ".concat(splittedFirst[0], " - ").concat(splittedFirst[1], " w ").concat(splittedSecond[0], " ").concat(splittedSecond[1]));
+        return true;
+    }
+    if ((Number(splittedSecond[1]) >= Number(splittedFirst[0])) && Number((splittedSecond[1]) <= Number(splittedFirst[1]))) {
+        console.log("A ".concat(splittedFirst[0], " - ").concat(splittedFirst[1], " w ").concat(splittedSecond[0], " ").concat(splittedSecond[1]));
         return true;
     }
     return false;
--- 4/index.ts
+++ 4/index.ts
@@ -5,14 +5,19 @@ const file = readFileSync("./data.txt", "utf-8");
 
 const splitted = file.split("\n");
 let result = 0;
+let result2 = 0;
 
 splitted.forEach(element => {
    if(checkPairs(element)){
      result++
    }
+   if(checkIfAnyDuplicate(element)){
+     result2++
+   }
 })
 
 console.log(result)
+console.log(result2)
 
 function checkPairs(element){
     const parts = element.split(',');
@@ -22,11 +27,38 @@ function checkPairs(element){
 
     if((Number(splittedFirst[0]) >= Number(splittedSecond[0])) && Number((splittedFirst[1]) <= Number(splittedSecond[1])))
     {
-        console.log(`A ${splittedFirst[0]} - ${splittedFirst[1]} w ${splittedSecond[0]} ${splittedSecond[1]}`)
         return true
     } 
     if((Number(splittedSecond[0]) >= Number(splittedFirst[0])) && Number((splittedSecond[1]) <= Number(splittedFirst[1]))){
-        console.log(`B ${splittedFirst[0]} - ${splittedFirst[1]} w ${splittedSecond[0]} ${splittedSecond[1]}`)
+        return true
+    } 
+    return false
+}
+
+function checkIfAnyDuplicate(element){
+    const parts = element.split(',');
+    const splittedFirst: number[] = parts[0].split('-')
+    const splittedSecond: number[] = parts[1].split('-')
+
+
+    if((Number(splittedFirst[0]) >= Number(splittedSecond[0])) && Number((splittedFirst[0]) <= Number(splittedSecond[1])))
+    {
+        console.log(`A ${splittedFirst[0]} - ${splittedFirst[1]} w ${splittedSecond[0]} ${splittedSecond[1]}`)
+        return true
+    } 
+    if((Number(splittedFirst[1]) >= Number(splittedSecond[0])) && Number((splittedFirst[1]) <= Number(splittedSecond[1])))
+    {
+        console.log(`A ${splittedFirst[0]} - ${splittedFirst[1]} w ${splittedSecond[0]} ${splittedSecond[1]}`)
+        return true
+    } 
+    if((Number(splittedSecond[0]) >= Number(splittedFirst[0])) && Number((splittedSecond[0]) <= Number(splittedFirst[1])))
+    {
+        console.log(`A ${splittedFirst[0]} - ${splittedFirst[1]} w ${splittedSecond[0]} ${splittedSecond[1]}`)
+        return true
+    } 
+    if((Number(splittedSecond[1]) >= Number(splittedFirst[0])) && Number((splittedSecond[1]) <= Number(splittedFirst[1])))
+    {
+        console.log(`A ${splittedFirst[0]} - ${splittedFirst[1]} w ${splittedSecond[0]} ${splittedSecond[1]}`)
         return true
     } 
     return false
